acab7f45378853d0805e0422c90b4c92817d5f6f
20191663 <ihs20191663@gmail.com>
2022-08-04 23:36:40 +0900

    3_리스트 사용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "하건영",
+    content: "hello1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "김건영",
+    content: "hello2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "이건영",
+    content: "hello3",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "박건영",
+    content: "hello4",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
