b8063ed67beb6f564f6b808f555d97eee7aad3bf
20191663 <ihs20191663@gmail.com>
2022-08-05 00:27:38 +0900

    4_리스트 데이터 추가하기

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import "./App.css";
+import React, { useState, useRef } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "하건영",
-    content: "hello1",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "김건영",
-    content: "hello2",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "이건영",
-    content: "hello3",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "박건영",
-    content: "hello4",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
-
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
